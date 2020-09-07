445a7ee38c0314308ff6d92584346eef11bdbe75
ScatterBrain-I <adalwulf23@gmail.com>
2020-09-07 17:12:27 -0400

    Add files via upload

--- OOP and Advanced Java/RectangleClass.java
+++ OOP and Advanced Java/RectangleClass.java
@@ -1,15 +1,19 @@
-/*
-Professor Desanty, I interpreted the UML so my class does not resemble it
-exactly. The UML had a function Rectangle() as well as Rectangle(int w, int h).
-I only need one constructor as so left out the first. I also anticpated The
-function getParameter() to be getPerimmiter() and so coded appropriately.
-I hope my assumptions were not presumptuous.
-*/
+/**
+ *
+ * @author PeterNiemeyer
+ * Assignment:Lab.Classes
+ * Submitted: 8 Sept 20
+ *
+ **/
 
 public class Rectangle{
   private int m_width;
   private int m_height;
 
+  public Rectangle(){
+    m_width = 2;
+    m_height = 3;
+  }
   public Rectangle(int w, int h){
     m_width = w;
     m_height = h;
