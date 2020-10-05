cab0e69037407a938066a0b8cbccfd05b3e9f1b3
ScatterBrain-I <adalwulf23@gmail.com>
2020-10-04 23:48:10 -0400

    in progress mergesort

---
+++ Algorithm/MergeSort/src/Main.java
@@ -0,0 +1,94 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main{
+  public static void main(String args[]){
+    // -----------------------------------
+    // do not edit main...
+    // -----------------------------------
+    List<Person> people = new ArrayList<Person>();
+
+    people.add(new Person("Frank", "Denton", 73));
+    people.add(new Person("Mark", "Cohen", 44));
+    people.add(new Person("Tim", "Smith", 22));
+    people.add(new Person("Steve", "Denton", 16));
+    people.add(new Person("Andy", "Ashton", 44));
+    people.add(new Person("Albert", "Denton", 58));
+
+    List<Person> ans = mergeSort(people);
+    System.out.println(ans);
+  }
+
+  public static List<Person> mergeSort(List<Person> items){
+    List<Person> sorted = new ArrayList<Person>();
+
+    if (items.size() == 1){
+      //sorted.add(items.get(0));
+      sorted = items;
+      System.out.println("BaseCase" + sorted); //Debug line
+
+    }
+    else {
+      //create two sublists
+      List<Person> listA = new ArrayList<Person>();
+      List<Person> listB = new ArrayList<Person>();
+      List<Person> sortedA = new ArrayList<Person>();
+      List<Person> sortedB = new ArrayList<Person>();
+
+      for (int i = 0; i < items.size(); i++){
+        if (i % 2 == 0){
+          listA.add(items.get(i));
+        }
+        else {
+          listB.add(items.get(i));
+        }
+      }
+      System.out.println("list A: " + listA); //Debug line
+      System.out.println("list B: " + listB); //Debug line
+      System.out.println();
+      //sort both sublists
+      mergeSort(listA);
+      mergeSort(listB);
+      //merge two lists
+      merge(listA, listB);
+    }
+    return sorted;
+  }
+
+  private static List<Person> merge(List<Person> partA, List<Person> partB){
+    List<Person> merged = new ArrayList<Person>();
+
+    while (partA.size() > 0 && partB.size() > 0){
+      int indexA = 0;
+      int indexB = 0;
+      if (partA.get(indexA).compareTo(partB.get(indexB)) < 0){
+        merged.add(partA.get(indexA));
+        indexA++;
+      }
+      /*else if (partA.get(indexA).compareTo(partB.get(indexB)) == 0){
+        if(partA.get(indexA).getFirst().compareTo(partB.get(indexB).getFirst()) == -1){
+          merged.add(partA.get(indexA));
+          indexA++;
+        }
+        else{
+          merged.add(partB.get(indexB));
+          indexB++;
+        }
+      }*/
+      else{
+        merged.add(partB.get(indexB));
+        indexB++;
+      }
+    }
+
+    if (partA.size() > 0){
+      merged.add(partA.get(0));
+    }
+
+    if (partB.size() > 0){
+      merged.add(partB.get(0));
+    }
+
+  return merged;
+  }
+}
---
+++ Algorithm/MergeSort/src/Person.java
@@ -0,0 +1,33 @@
+public class Person implements Comparable <Person>{
+  private String m_first;
+  private String m_last;
+  private int m_age;
+
+  public Person(String first, String last, int age){
+    m_first = first;
+    m_last = last;
+    m_age = age;
+  }
+
+  public String getFirst(){
+    return m_first;
+  }
+
+  public String getLast(){
+    return m_last;
+  }
+
+  public int getAge(){
+    return m_age;
+  }
+
+  @Override
+  public int compareTo(Person other){
+    return m_last.compareTo(other.m_last);
+  }
+
+  @Override
+  public String toString(){
+    return m_last + ", " + m_first + " " + m_age;
+  }
+}
