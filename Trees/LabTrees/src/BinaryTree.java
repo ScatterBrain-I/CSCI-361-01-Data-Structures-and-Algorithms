f9073f170684ebc038fb81443101cae67042e344
ScatterBrain-I <adalwulf23@gmail.com>
2020-10-28 22:56:54 -0400

    Finished

--- Trees/LabTrees/src/BinaryTree.java
+++ Trees/LabTrees/src/BinaryTree.java
@@ -1,100 +1,89 @@
-public class BinaryTree implements Comparable<BinaryTree>
-{
-    private String m_value;
-    private BinaryTree m_left;
-    private BinaryTree m_right;
+public class BinaryTree<T extends Comparable<T>> implements Comparable<BinaryTree<T>> {
 
-    public SimpleTree(String value)
-    {
+    private T m_value;
+    private BinaryTree<T> m_left;
+    private BinaryTree<T> m_right;
+
+    public BinaryTree(T value) {
         m_value = value;
         m_left = null;
         m_right = null;
     }
 
-    public SimpleTree(String value,
-                      BinaryTree left,
-                      BinaryTree right)
-    {
+    public BinaryTree(T value, BinaryTree<T> left, BinaryTree<T> right) {
         m_value = value;
         m_left = left;
         m_right = right;
     }
 
-    public BinaryTree getLeft()
-    {
+    public BinaryTree<T> getLeft() {
         return m_left;
     }
 
-    public BinaryTree getRight()
-    {
+    public BinaryTree<T> getRight() {
         return m_right;
     }
 
-    public String getValue()
-    {
+    public T getValue() {
         return m_value;
     }
 
-    public void setLeft(BinaryTree left)
-    {
+    public void setLeft(BinaryTree<T> left) {
         m_left = left;
     }
 
-    public void setRight(BinaryTree right)
-    {
+    public void setRight(BinaryTree<T> right) {
         m_right = right;
     }
 
-    public void setVale(String value)
-    {
+    public void setValue(T value) {
         m_value = value;
     }
 
-    @Override
-    public String toString()
-    {
+    public String toString() {
         StringBuffer buf = new StringBuffer();
         inorder(buf);
         return buf.toString();
     }
 
-    public void inorder(StringBuffer buf)
-    {
-        if (m_left != null)
+    public void inorder(StringBuffer buf) {
+        if (m_left != null) {
             m_left.inorder(buf);
+        }
 
-        buf.append("\n"+ m_value);
+        buf.append("\n " + m_value);
 
-        if (m_right != null)
+        if (m_right != null) {
             m_right.inorder(buf);
+        }
     }
 
-    public void preorder(StringBuffer buf)
-    {
-        buf.append("\n"+ m_value);
+    public void preorder(StringBuffer buf) {
+        buf.append("\n " + m_value);
 
-        if (m_left != null)
+        if (m_left != null) {
             m_left.preorder(buf);
+        }
 
-        if (m_right != null)
+        if (m_right != null) {
             m_right.preorder(buf);
+        }
     }
 
-    public void postorder(StringBuffer buf)
-    {
-        if (m_left != null)
+    public void postorder(StringBuffer buf) {
+        if (m_left != null) {
             m_left.postorder(buf);
+        }
 
-        if (m_right != null)
+        if (m_right != null) {
             m_right.postorder(buf);
+        }
 
-        buf.append("\n"+ m_value);
+        buf.append("\n" + m_value);
     }
 
     @Override
-    public int compareTo(BinaryTree other)
-    {
+    public int compareTo(BinaryTree<T> other) {
         return m_value.compareTo(other.m_value);
     }
-
 }
--- Trees/LabTrees/src/Test.java
+++ Trees/LabTrees/src/Test.java
@@ -1,26 +1,26 @@
-import java.util.*;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Test
-{
-    public static void main(String args[])
-    {
+public class Test{
+    public static void main(String args[]){
         /*
                 A
             B       C
          D     E       F
         */
-        BinaryTree treeD = new SimpleTree("D");
-        BinaryTree treeE = new SimpleTree("E");
-        BinaryTree treeF = new SimpleTree("F");
-        BinaryTree treeB = new SimpleTree("B", treeD, treeE);
-        BinaryTree treeC = new SimpleTree("C", null, treeF);
-        BinaryTree root = new SimpleTree("A", treeB, treeC);
+        BinaryTree<String> treeD = new BinaryTree<String>("D");
+        BinaryTree<String> treeE = new BinaryTree<String>("E");
+        BinaryTree<String> treeF = new BinaryTree<String>("F");
+        BinaryTree<String> treeB = new BinaryTree<String>("B", treeD, treeE);
+        BinaryTree<String> treeC = new BinaryTree<String>("C", null, treeF);
+        BinaryTree<String> root = new BinaryTree<String>("A", treeB, treeC);
 
         // D B E A C F
         String s = root.toString();
         System.out.println(s);
 
-        List<BinaryTree> trees = new ArrayList<BinaryTree>();
+        List<BinaryTree<String>> trees = new ArrayList<BinaryTree<String>>();
         trees.add(treeD);
         trees.add(treeF);
         trees.add(treeC);
@@ -30,8 +30,41 @@ public class Test
 
         Collections.sort(trees);
 
-        for (BinaryTree t : trees)
-        {
+        for (BinaryTree t : trees){
+            System.out.println();
+            System.out.print(t.getValue() + " ");
+            System.out.println();
+        }
+
+        /*          9
+                5       13
+            2       7       17
+        1
+        */
+        BinaryTree<Double> tree1 = new BinaryTree<Double>(1.0);
+        BinaryTree<Double> tree2 = new BinaryTree<Double>(2.2, tree1, null);
+        BinaryTree<Double> tree7 = new BinaryTree<Double>(7.3);
+        BinaryTree<Double> tree17 = new BinaryTree<Double>(17.9);
+        BinaryTree<Double> tree5 = new BinaryTree<Double>(5.2, tree2, tree7);
+        BinaryTree<Double> tree13 = new BinaryTree<Double>(13.1, null, tree17);
+        BinaryTree<Double> root9 = new BinaryTree<Double>(9.9, tree5, tree13);
+
+        // 1 2 5 7 9 13 17
+        String s2 = root9.toString();
+        System.out.println(s2);
+
+        List<BinaryTree<Double>> treeDoub = new ArrayList<BinaryTree<Double>>();
+        treeDoub.add(tree1);
+        treeDoub.add(tree7);
+        treeDoub.add(tree17);
+        treeDoub.add(tree13);
+        treeDoub.add(root9);
+        treeDoub.add(tree2);
+        treeDoub.add(tree5);
+
+        Collections.sort(treeDoub);
+
+        for (BinaryTree t : treeDoub){
             System.out.println();
             System.out.print(t.getValue() + " ");
             System.out.println();
