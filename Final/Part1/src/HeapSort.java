d76a4f964dc478c13502c38d5e38f99f9e08a615
ScatterBrain-I <adalwulf23@gmail.com>
2020-12-12 13:47:29 -0500

    exam files

--- Final/Part1/src/HeapSort.java
+++ Final/Part1/src/HeapSort.java
@@ -2,6 +2,12 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class HeapSort<T> {
+    private static List<Integer> result;
+
+    public HeapSort(List<Integer> items) {
+        sortHeap(items);
+    }
+
     public static List<Integer> sortHeap(List<Integer> items) {
         List<Integer> nodes = new ArrayList<Integer>();
         Heap<Integer> maxHeap = new Heap<Integer>();
@@ -9,6 +15,12 @@ public class HeapSort<T> {
         while (maxHeap.size() > 0) {
             nodes.add(0, maxHeap.removeTop());
         }
-        return nodes;
+        result = nodes;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return result.toString();
     }
 }
--- Final/Part1/src/Main.java
+++ Final/Part1/src/Main.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // List of inputs
+        // List of inputs for test
         List<Integer> list = new ArrayList<>(Arrays.asList(6,5,9,2));
 
-        // make new object - call sorting method - print result 2,5,6,9 expected
-        HeapSort<Integer> examTest = new HeapSort<Integer>();
-        List<Integer> result = examTest.sortHeap(list);
-        System.out.println(result);
+        HeapSort<Integer> examTest = new HeapSort<Integer>(list);
+        System.out.println(examTest);
     }
 }
